852fc38ce9407eccffccc87523c7ea6760103e7e
Martun <65818128+martun-avagyan@users.noreply.github.com>
2022-03-17 19:27:38 +0400

    feat: add number of cart items functionality

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
-
+import CartContext from "../../Store/cart-context";
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button onClick={props.onClick} className={classes.button}>
       <span className={classes.CartIcon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>7</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
